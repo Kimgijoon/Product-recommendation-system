989254ff0091cb98fc5258008e072f267d74447e
gijoonKim <kozx1212@gmail.com>
2021-04-03 17:07:52 +0900

    feat: 상품 정보 받아오는 함수 구현
    2021.04.03 요청 많이하면 429 에러를 리턴하는 경우 확인
    sleep 랜덤으로 걸어서 기다리다가 데이터 받아올 때까지 recursive call로 request 보냄

---
+++ util/crawler/naver_crawler.py
@@ -0,0 +1,47 @@
+import time
+import json
+import requests
+
+from typing import List, Dict, Tuple, Optional
+
+from util.crawler.base_crawler import BaseCrawler
+
+
+class NaverShoppingCrawler(BaseCrawler):
+
+	def __init__(self, category_name: str):
+
+		self.category_name = category_name
+
+	def _get_html(self, url: str) -> Tuple[Dict[str, str], List[str]]:
+		"""json 받아오는 함수, 429 error 발생 시 response 받아올 때까지 recursive call
+    Args:
+      url:  target url
+    Returns:
+      dic:  response data
+    """
+    r = requests.get(url, timeout=self.timeout)
+    current_url: str = r.url
+    dic: Dict[str, str] = json.loads(r.text)
+    
+    if r.status_code == 429:
+      time.sleep(random.randint(10, 30))
+      return self._get_html(url)
+
+    return dic, current_url
+
+  def parse_main_html(self, url: str) -> Tuple[List[Dict[str, Optional[str]]], List[str], List[str]]:
+		"""메인 url로 부터 meta 및 sub url을 긁는 함수
+    Args:
+      url:  target url
+    Returns:
+      meta_list:  list contains meta data
+      sub_url_list: list contains sub url
+      date_list:  list contains creation date
+    """
+		res, _ = self._get_html(url)
+    data = res['shoppingResult']['products']
+
+	def parse_sub_html(self):
+
+		pass
