b102744d0adb5cb7139d1ed5e62be324d4ea8452
gijoonKim <kozx1212@gmail.com>
2021-04-03 18:49:53 +0900

    feat: 서브 페이지에서 댓글 수집하는 함수 구현
    2021.04.03 댓글, 작성자, 작성일자, 점수 등을 수집함

--- util/crawler/naver_crawler.py
+++ util/crawler/naver_crawler.py
@@ -1,3 +1,4 @@
+import re
 import time
 import json
 import requests
@@ -8,13 +9,13 @@ from util.crawler.base_crawler import BaseCrawler
 
 
 class NaverShoppingCrawler(BaseCrawler):
-
-	def __init__(self, category_name: str):
-
-		self.category_name = category_name
-
-	def _get_html(self, url: str) -> Tuple[Dict[str, str], List[str]]:
-		"""json 받아오는 함수, 429 error 발생 시 response 받아올 때까지 recursive call
+  
+  def __init__(self, category_name: str):
+    
+    self.category_name = category_name
+    
+  def _get_html(self, url: str) -> Tuple[Dict[str, str], List[str]]:
+    """json 받아오는 함수, 429 error 발생 시 response 받아올 때까지 recursive call
     Args:
       url:  target url
     Returns:
@@ -27,11 +28,11 @@ class NaverShoppingCrawler(BaseCrawler):
     if r.status_code == 429:
       time.sleep(random.randint(10, 30))
       return self._get_html(url)
-
+      
     return dic, current_url
-
+    
   def parse_main_html(self, url: str) -> Tuple[List[Dict[str, Optional[str]]], List[str], List[str]]:
-		"""메인 url로 부터 meta 및 sub url을 긁는 함수
+    """메인 url로 부터 meta 및 sub url을 긁는 함수
     Args:
       url:  target url
     Returns:
@@ -41,11 +42,11 @@ class NaverShoppingCrawler(BaseCrawler):
     """
 		res, _ = self._get_html(url)
     data = res['shoppingResult']['products']
-
+    
     meta_list: List[Dict[str, Optional[str]]] = []
     sub_url_list: List[str] = [] 
     date_list: List[str] = []
-    for idx, dic in enumerate(data):
+    for dic in data:
       if dic['lowMallList'] == None:  continue
       else:
         sub_url = dic.get('crUrl', None)
@@ -73,6 +74,66 @@ class NaverShoppingCrawler(BaseCrawler):
 
     return meta_list, sub_url_list, date_list
 
-	def parse_sub_html(self):
+  def _parse_review_html(self, url: str, prod_name: str) -> List[Dict[str, Optional[str]]]:
+    """sub url에서 댓글을 긁는 함수
+    Args:
+      url:  target url
+    Returns:
+      comment_list: list contains comment, user id, create date and score
+    """
+    res, _ = self._get_html(url)
+    reviews = res.get('reviews', None)
 
-		pass
+    regex = re.compile(r'<[^>]+>')
+    comment_list: List[Dict[str, Optional[str]]] = []
+    for i in reviews:
+      review = regex.sub('', i['content'])
+      id = f"{i['mallName']}_{i['userId']}"
+      date = i['registerDate']
+      score = i['starScore']
+      comment_list.append({
+        'comment': review,
+        'user_id': id,
+        'create_date': date,
+        'score': score,
+        'prod_name': prod_name,
+        'category': self.category_name
+      })
+
+    return comment_list
+    
+  def parse_sub_html(self):
+    """sub url에서 전체 댓글을 긁는 함수
+    Args:
+      url:  target url
+    Returns:
+      comment_list: list contains multiple data
+    """
+    url_id: int = url.split('nvMid=')[1].split('&')[0]
+    url: str = 'https://search.shopping.naver.com/api/review?nvMid=' + str(url_id) + \
+              '&reviewType=ALL&sort=QUALITY&isNeedAggregation=N&isApplyFilter=N&page=1&pageSize=20'
+    try:
+      res, _ = self._get_html(url)
+      total_count = res['totalCount']
+    except Exception as e:
+      print(f'err: {e}')
+      return None
+
+    if (total_count // 1000) == 0:
+      max_count = (total_count // 20) + 1
+    else:
+      max_count = total_count // 20
+
+    comment_list = []
+    for page_num in range(1, max_count):
+      review_url = 'https://search.shopping.naver.com/api/review?nvMid=' + str(url_id) + \
+                  '&reviewType=ALL&sort=QUALITY&isNeedAggregation=N&isApplyFilter=N&page=' + \
+                  str(page_num) + '&pageSize=1000'
+
+      try:
+        comments = self._parse_review_html(review_url, prod_name)
+        comment_list.extend(comments)
+      except:
+        pass
+
+    return comment_list
