13ec2b0baa6802f371ad38bfee7ad065b988ccf6
gijoonKim <kozx1212@gmail.com>
2021-04-04 17:47:23 +0900

    feat: mongodb에서 댓글 가져오는 함수 구현
    2021.04.04 특정 조건에 부합하는 review를 return

--- util/db_util.py
+++ util/db_util.py
@@ -34,3 +34,38 @@ class MongoController(object):
     else:
       for dic in db_collection.find(query): result.append(dic)
     return result
+
+  def _get_user_list(self, dic_list: List[Dict[str, Any]]) -> List[str]:
+    """id에서 비식별화되지 않은 문자가 3개이상인 user id를 return하는 함수
+    Args:
+      dic_list: user id가 담겨있는 dict
+    Return:
+      suer_list:  selected user list
+    """
+    user_list: List = [x['user_id'] for x in dic_list]
+    dic: Dict[str, int] = dict(collections.Counter(user_list))
+
+    user_list: List[str] = []
+    for i in dic:
+      if (self.p.match(i)) and (dic[i] > 100):
+        user_list.append(i)
+
+    return user_list
+
+  def get_review(self, tb_name: str) -> List[Dict[str, Any]]:
+    """특정 조건에 맞는 리뷰를 return하는 함수
+    Args:
+      tb_name:  collection name
+    Return:
+      selected_review_list: review list for specific conditions
+    """
+    dic_list: List[Dict[str, Any]] = self.select_data(tb_name)
+    user_list: List[Dict[str, Any]] = self._get_user_list(dic_list)
+
+    selected_review_list: List[Dict[str, Any]] = []
+    for idx, user_id in enumerate(user_list):
+      if idx == 1: break
+      res = [x for x in dic_list if x['user_id'] == user_id]
+      selected_review_list.extend(res)
+
+    print(selected_reivew_list[:5])
