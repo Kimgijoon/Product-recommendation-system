0400165890a777afecd82c6db767ea8f2ff64bc6
gijoonKim <kozx1212@gmail.com>
2021-04-17 14:43:41 +0900

    feat: main파일 수정
    2021.04.17 mongodb 관련 함수들 쓸 수 있게 추가

--- main.py
+++ main.py
@@ -11,13 +11,16 @@ flags.mark_flag_as_required('op')
 tf.flags.DEFINE_string('category', None, 'Name of category')
 tf.flags.DEFINE_string('config_path', 'configs', 'directory of config file')
 tf.flags.DEFINE_string('config_file', 'category.json', 'config file name')
+tf.flags.DEFINE_string('id', None, 'mongodb id')
+tf.flags.DEFINE_string('passwd', None, 'mongodb password')
 
 
 def main():
 
   if FLAGS.Op == 'crawler':
+    from util.db_util import MongoController
     from util.crawler.naver_crawler import NaverShoppingCrawler
-
+    
     category_dic_path = f'{FLAGS.config_path}/{FLAGS.config_file}'
     with open(category_dic_path, 'r') as f:
       category_dic = json.loads(f.read())
@@ -26,6 +29,7 @@ def main():
     category_id = category_dic.get(category_name, None)
 
     crawler = NaverShoppingCrawler(category_name, 60)
+    db = MongoController(FLAGS.id, FLAGS.passwd)
 
     for page_num in range(1, 50):
       url = 'https://search.shopping.naver.com/api/search/category?sort=rel&pagingIndex={}&pagingSize=40&viewType=list&productSet=total&catId={}&deliveryFee=&deliveryTypeValue=&iq=&eq=&xq=&frm=NVSHTTL&window='
@@ -36,7 +40,9 @@ def main():
         comment_list = crawler.parse_sub_html(sub_url_list[idx], meta_list[idx]['name'])
         if (len(comment_list) == 0) or (comment_list == None):
           continue
+        db.insert_data(comment_list, 'review')
         time.sleep(random.randint(10, 30))
+      db.insert_data(meta_list, FLAGS.category)
 
 
 if __name__ == '__main__':
